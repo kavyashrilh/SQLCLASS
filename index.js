c8caac04465ca908a964cb9d784f705e4dd7a6e8
Kavyashri LH <kavyashrilh@gmail.com>
2026-03-23 20:27:06 +0530

    sql

---
+++ index.js
@@ -0,0 +1,116 @@
+const { faker } = require('@faker-js/faker');
+const mysql=require("mysql2");
+const express=require("express");
+const app=express();
+const path=require("path");
+const methodOverride=require("method-override");
+
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({extended:true}));
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
+
+const connection = mysql.createConnection({
+    host:'localhost',
+  user: 'root',
+  database: 'delta_app',
+  password: '21012005'
+});
+
+let  getRandomUser=()=> {
+  return [
+   faker.string.uuid(),
+     faker.internet.username(), // before version 9.1.0, use userName()
+    faker.internet.email(),
+    faker.internet.password(),
+  ];
+
+};
+
+
+
+//home route
+app.get("/",(req,res)=>{
+  let q=`SELECT count(*) FROM user`;
+  try{
+    connection.query(q, (err,result)=>{
+    if (err) throw err;
+    let count=result[0]["count(*)"];
+   
+    console.log(result[0]["count(*)"]);
+     res.render("home.ejs",{count});
+});
+} catch(err){
+    console.log(err);
+    res.send("some error");
+}
+ 
+});
+
+
+//show route
+app.get("/user",(req,res)=>{
+   let q=`SELECT * FROM user`;
+    try{
+    connection.query(q, (err,users)=>{
+    if (err) throw err;
+    res.render("showusers.ejs",{users});
+});
+} catch(err){
+    console.log(err);
+    res.send("some error");
+}
+});
+
+app.get("/user/:id/edit",(req,res)=>{
+  let {id}=req.params;
+  let q=`SELECT * FROM user WHERE id='${id}'`;
+  
+    try{
+    connection.query(q, (err,result)=>{
+    if (err) throw err;
+    console.log(result);
+    let user=result[0];
+   res.render("edit.ejs",{user});
+});
+} catch(err){
+    console.log(err);
+    res.send("some error");
+}
+});
+
+app.patch("/user/:id",(req,res)=>{
+    let {id}=req.params;
+    let{password:formPass,username:newUsername}=req.body;
+  let q=`SELECT * FROM user WHERE id='${id}'`;
+  
+    try{
+    connection.query(q, (err,result)=>{
+    if (err) throw err;
+    // console.log(result);
+    let user=result[0];
+    if(formPass !=user.password){
+      res.send("worng password");
+    }else{
+      let q2=`UPDATE user SET username='${newUsername}' WHERE id='${id}'`;
+      connection.query(q2,(err,result)=>{
+        if (err) throw err;
+        res.redirect("/user");
+      })
+    }
+    
+}); 
+} catch(err){
+    console.log(err);
+    res.send("some error");
+}
+});
+
+app.de
+
+app.listen("8080",()=>{
+  console.log("server is listening to port 8080");
+});
+
+
+
